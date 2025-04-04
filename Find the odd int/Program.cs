5909b7b37dbaf92753b8a49ed318333c1b23528c
anillen <daniil_telenkov@vk.com>
2025-04-04 08:43:35 +0300

    add: Find the odd int

---
+++ Find the odd int/Program.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+
+namespace Solution
+{
+    class Kata
+    {
+        public static int find_it(int[] seq) 
+        {
+            return seq.GroupBy(x=>x).Where(x=>(x.Count() % 2 != 0)).First().Key;
+        }
+    }
+}
