a2e132a44b54060a70f1456cadcaee847f2b0004
anillen <daniil_telenkov@vk.com>
2025-04-15 08:48:49 +0300

    add: Reversed Strings

---
+++ Reversed Strings/Program.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string Solution(string str) 
+    {
+        return string.Join("",str.ToArray().Reverse());
+    }
+}
