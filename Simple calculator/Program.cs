984d348617af5afeb19a7b12ca04c9b80a8128f7
anillen <daniil_telenkov@vk.com>
2025-07-21 09:19:48 +0300

    add: Simple calculator

---
+++ Simple calculator/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Kata
+{
+  public static double Calculator(double a, double b, char op)
+  {
+    switch(op)
+    {
+        case '+': return a+b;
+        case '-':return a-b;
+        case '*': return a*b;
+        case '/':return a/b;
+        default: throw new ArgumentException();
+    }
+  }
+}
