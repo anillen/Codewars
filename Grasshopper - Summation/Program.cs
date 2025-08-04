1fe1e804d0a5245dafcbb545e27452e513701c96
anillen <daniil_telenkov@vk.com>
2025-08-04 08:46:15 +0300

    add: Grasshopper - Summation

---
+++ Grasshopper - Summation/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+public static class Kata 
+{
+    public static int summation(int num)
+    {
+      int result = 0;
+      for(int i = 1; i <= num; i++)
+        result+=i;
+      return result;
+    }
+}
